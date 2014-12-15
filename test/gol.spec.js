0df2822c1fae3eb6d6f34a0a0c1240786a8ae5e8
tompascall <tompascall@gmail.com>
2014-12-15 12:21:06 +0100

    first test fails

--- test/gol.spec.js
+++ test/gol.spec.js
@@ -2,4 +2,14 @@
 'use strict';
 
 var expect = require('expect.js');
+var helper = require('../lib/testHelper/expectHelper.js');
 var gol = require('../src/gol.js');
+
+describe('world validation', function(){
+  it('should throw exception if "world" object argument is missing', function(){
+    var message = '"world" argument is missing';
+    if (!helper.testExceptionMessage(message, gol.nexGen)) {
+      expect().fail();
+    }
+  });
+});
