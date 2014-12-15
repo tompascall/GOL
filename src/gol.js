ab0362710152dc832c3b8ede35dce6cdc5982c3a
tompascall <tompascall@gmail.com>
2014-12-15 16:35:14 +0100

    validating "world" argument

--- src/gol.js
+++ src/gol.js
@@ -4,5 +4,43 @@
 
 var gol = {};
 
+gol.validateWorldMissing = function() {
+  if (gol.world === undefined) {
+    throw new Error('Error: "world" argument is missing');
+  }
+};
+
+gol.validateWorldIsObject = function() {
+  if (!(gol.world instanceof Object)) {
+    throw new Error('Error: world argument must be an object');
+  }
+};
+
+gol.validateWorldHasKey = function(key) {
+  var keys = Object.keys(gol.world);
+  if (keys.indexOf(key) === -1) {
+    throw new Error('Error: "world" object has no "' + key + '" property');
+  }
+};
+
+gol.validateWorldTypeOfKey = function(key, type) {
+  if (typeof gol.world[key] !== type) {
+    throw new Error('Error: "' + key + '" property must be a(n) '+ type);
+  }
+};
+
+gol.validateWorld = function() {
+  gol.validateWorldMissing();
+  gol.validateWorldIsObject();
+  gol.validateWorldHasKey('beings');
+  gol.validateWorldTypeOfKey('beings', 'array');
+};
+
+gol.nextGen = function(world) {
+  gol.world = world;
+  gol.validateWorld();
+  return gol.world;
+};
+
 module.exports = gol;
 
--- test/gol.spec.js
+++ test/gol.spec.js
@@ -6,10 +6,26 @@ var helper = require('../lib/testHelper/expectHelper.js');
 var gol = require('../src/gol.js');
 
 describe('world validation', function(){
-  it('should throw exception if "world" object argument is missing', function(){
-    var message = '"world" argument is missing';
-    if (!helper.testExceptionMessage(message, gol.nexGen)) {
-      expect().fail();
-    }
+  it('should throw exception if "world" argument is missing', function(){
+    var message = 'Error: "world" argument is missing';
+    expect(helper.testExceptionMessage(message, gol.nextGen)).to.be(true);
+  });
+
+  it('should throw exception if "world" argument is not an object', function(){
+    var message = 'Error: world argument must be an object';
+    var world = 'foo';
+    expect(helper.testExceptionMessage(message, gol.nextGen, world)).to.be(true);
+  });
+
+  it('should throw exception if "world" object has no "beings" property', function(){
+    var message = 'Error: "world" object has no "beings" property';
+    var world = {};
+    expect(helper.testExceptionMessage(message, gol.nextGen, world)).to.be(true);
+  });
+
+  it('should throw exception if "being" property is not an array', function(){
+    var message = 'Error: "beings" property must be a(n) array';
+    var world = {beings : ''};
+    expect(helper.testExceptionMessage(message, gol.nextGen, world)).to.be(true);
   });
 });
