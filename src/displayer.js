4f375c4e9c7fa8591715b46a9fbe1f7a058d2358
tompascall <tompascall@gmail.com>
2014-12-17 21:51:41 +0100

    starting displayer

---
+++ src/displayer.js
@@ -0,0 +1,71 @@
+// displayer.js
+
+'use strict';
+
+var gol = require('../src/gol.js');
+
+var beingsMap = {
+  map: [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0,
+    0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  ],
+  width: 38,
+  height: 11,
+  startX: 0,
+  startY: 0
+};
+
+function Scope(width, height){
+  this.width = width,
+  this.height = height,
+  this.stringified = this.clear()
+};
+
+Scope.prototype.clear = function() {
+  var scope = '';
+  for (var i = 1; i <= this.height; i++) {
+    for (var j = 1; j <= this.width; j++) {
+      scope += '.';
+      if (j === this.width) {
+        scope += '\n';
+      }
+    }
+  }
+  return scope;
+};
+
+var scope = new Scope(40, 20);
+console.log(scope.stringified);
+
+// var world = new gol.World();
+// world.loadBeingsMap(beingsMap);
+// var nextGeneration;
+
+// animate();
+
+// function animate(){
+//   //display();
+//   setTimeout(function(){
+//     world = gol.nextGen(world);
+//     //scope.setScopeByTable(world);
+//     clearDisplay();
+//     animate();
+//   }, 50);
+
+//   function clearDisplay(){
+//     console.log('\n\n\n\n\n\n\n\n\n\n' +
+//                 '\n\n\n\n\n\n\n\n\n\n' +
+//                 '\n\n\n\n\n\n\n\n\n\n' +
+//                 '\n\n\n\n\n\n\n\n\n\n' +
+//                 '\n\n\n\n\n\n\n\n\n\n');
+//   }
+// }
